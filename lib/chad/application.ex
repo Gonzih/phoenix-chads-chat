195915d25405223cf2c1067889a371fbf17fd1d6
Max Gonzih <gonzih@gmail.com>
2020-05-11 14:29:14 -0400

    Chads dont need no database

--- lib/chad/application.ex
+++ lib/chad/application.ex
@@ -8,7 +8,7 @@ defmodule Chad.Application do
   def start(_type, _args) do
     children = [
       # Start the Ecto repository
-      Chad.Repo,
+      # Chad.Repo,
       # Start the Telemetry supervisor
       ChadWeb.Telemetry,
       # Start the PubSub system
